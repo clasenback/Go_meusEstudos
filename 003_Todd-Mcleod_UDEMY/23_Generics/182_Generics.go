a5ac477f7ce903aaec4e0dedc91521b25570859c
Alex Back <clasen.back.eng@gmail.com>
2023-10-10 19:24:44 -0300

    generics

---
+++ 003_Todd-Mcleod_UDEMY/23_Generics/182_Generics.go
@@ -0,0 +1,41 @@
+package main
+
+import "fmt"
+
+func main() {
+	var a int = 3
+	var b float32 = 3.14
+	fmt.Println(add1(a, a))
+	fmt.Println(add1(b, b))
+	// fmt.Println(add(a, b))
+
+	fmt.Println(add2(b, b))
+
+	var c meuInteiro = 4
+	s := add3(c, c)
+	fmt.Printf("%v\t%T\n", s, s)
+
+}
+
+func add1[T float32 | int](a, b T) T {
+	return a + b
+}
+
+type meuTipo interface {
+	float32 | int
+}
+
+func add2[T meuTipo](a, b T) T {
+	return a + b
+}
+
+type meuTipo2 interface {
+	~float32 | ~int
+}
+
+//add3 recebe a e b de quaisquer tipos rastreáveis como float32 ou int e retorna a soma com o mesmo tipo que recebeu
+func add3[T meuTipo2](a, b T) T {
+	return a + b
+}
+
+type meuInteiro int
